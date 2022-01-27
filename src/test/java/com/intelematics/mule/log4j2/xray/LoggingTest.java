3ac8e4d8dea6d78fe12c080098650c7d8e5b2745
Julian West <julian.west@intelematics.com>
2022-01-28 10:44:29 +1100

    Disable the test

--- src/test/java/com/intelematics/mule/log4j2/xray/LoggingTest.java
+++ src/test/java/com/intelematics/mule/log4j2/xray/LoggingTest.java
@@ -4,10 +4,12 @@ import java.util.UUID;
 
 import org.apache.logging.log4j.LogManager;
 import org.joda.time.Instant;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class LoggingTest {
 	
+	@Disabled
 	@Test
 	public void testLogging() {
 		
