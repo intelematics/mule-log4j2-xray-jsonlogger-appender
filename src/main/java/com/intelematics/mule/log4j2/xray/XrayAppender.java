1b5b23a4820e39cd868808b61e4233c9ac4a47a8
Julian West <julian.west@intelematics.com>
2022-01-31 12:46:59 +1100

    Fix for un-ended blocks without a start. This should solve some hanging processes Formatting all code

--- src/main/java/com/intelematics/mule/log4j2/xray/XrayAppender.java
+++ src/main/java/com/intelematics/mule/log4j2/xray/XrayAppender.java
@@ -26,145 +26,143 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 @Plugin(name = "Xray", category = "Core", elementType = "appender", printObject = true)
 public class XrayAppender extends AbstractAppender {
 
-	private static final int EXPIRE_AFTER_SECONDS = 120;
+  private static final int EXPIRE_AFTER_SECONDS = 120;
 
-	private static Logger logger = LogManager.getLogger(XrayAppender.class);
+  private static Logger logger = LogManager.getLogger(XrayAppender.class);
 
-	static final Boolean DEBUG_MODE = System.getProperty("log4j.debug") != null;
-	static String JsonLoggerClass = "org.mule.extension.jsonlogger.JsonLogger";
+  static final Boolean DEBUG_MODE = System.getProperty("log4j.debug") != null;
+  static String JsonLoggerClass = "org.mule.extension.jsonlogger.JsonLogger";
 
-	/**
-	 * The queue used to buffer log entries
-	 */
-	private XrayAgent xrayAgent;
+  /**
+   * The queue used to buffer log entries
+   */
+  private XrayAgent xrayAgent;
 
-	private String awsRegion;
+  private String awsRegion;
 
-	private HashMap<String, JsonLoggerTransaction> transactions = new HashMap<>();
-	private TreeMap<Instant, String> transactionExpiry = new TreeMap<>();
+  private HashMap<String, JsonLoggerTransaction> transactions = new HashMap<>();
+  private TreeMap<Instant, String> transactionExpiry = new TreeMap<>();
 
-	private XrayAppender(final String name, final Layout layout, final Filter filter, final boolean ignoreExceptions,
-			final String awsRegion, Integer queueLength, Integer messagesBatchSize) {
+  private XrayAppender(final String name, final Layout layout, final Filter filter, final boolean ignoreExceptions, final String awsRegion, Integer queueLength, Integer messagesBatchSize) {
 
-		super(name, filter, layout, ignoreExceptions);
-		this.awsRegion = awsRegion == null ? System.getProperty("awsRegion") : awsRegion;
+    super(name, filter, layout, ignoreExceptions);
+    this.awsRegion = awsRegion == null ? System.getProperty("awsRegion") : awsRegion;
 
-		System.out.println("## Xray logging started  O.O"); // Can't use the logger here, as it is never setup right
-															// now.
-	}
+    System.out.println("## Xray logging started  O.O"); // Can't use the logger here, as it is never setup right
+    // now.
+  }
 
-	@Override
-	public void append(LogEvent event) {
-		try {
-			sendXrayEvent(event);
-		} catch (Exception e) {
-			logger.error("## Couldn't send to Xray", e);
-		}
-	}
+  @Override
+  public void append(LogEvent event) {
+    try {
+      sendXrayEvent(event);
+    } catch (Exception e) {
+      logger.error("## Couldn't send to Xray", e);
+    }
+  }
 
-	public void sendXrayEvent(LogEvent event) throws JsonMappingException, JsonProcessingException {
-		String loggerName = event.getLoggerName();
+  public void sendXrayEvent(LogEvent event) throws JsonMappingException, JsonProcessingException {
+    String loggerName = event.getLoggerName();
 
-		if (JsonLoggerClass.equals(loggerName)) {
+    if (JsonLoggerClass.equals(loggerName)) {
 
-			if (DEBUG_MODE)
-				logger.info("## Xray event found ");
-			JsonLoggerEntry entry = new JsonLoggerEntry(event.getMessage().getFormattedMessage());
-			JsonLoggerTransaction transaction = null, requestTransaction;
+      if (DEBUG_MODE)
+        logger.info("## Xray event found ");
+      JsonLoggerEntry entry = new JsonLoggerEntry(event.getMessage().getFormattedMessage());
+      JsonLoggerTransaction transaction = null, requestTransaction;
 
-			switch (entry.getTrace()) {
-			case START:
-				transaction = getTransaction(entry);
-				transaction.setStart(entry);
-				transactions.put(entry.getCorrelationId(), transaction);
-				break;
-			case BEFORE_REQUEST:
-				transaction = getTransaction(entry);
-				requestTransaction = transaction.addRequestTransaction();
-				requestTransaction.setStart(entry);
-				break;
-			case AFTER_REQUEST:
-				transaction = getTransaction(entry);
-				requestTransaction = transaction.getOpenRequestTransaction();
-				requestTransaction.setEnd(entry);
-				break;
-			case EXCEPTION:
-				transaction = getTransaction(entry);
-				transaction.addException(entry);
-				break;
-			case END:
-				transaction = getTransaction(entry);
-				transaction.setEnd(entry);
-				transactions.remove(entry.getCorrelationId());
-				break;
-			}
+      switch (entry.getTrace()) {
+      case START:
+        transaction = getTransaction(entry);
+        transaction.setStart(entry);
+        transactions.put(entry.getCorrelationId(), transaction);
+        break;
+      case BEFORE_REQUEST:
+        transaction = getTransaction(entry);
+        requestTransaction = transaction.addRequestTransaction();
+        requestTransaction.setStart(entry);
+        break;
+      case AFTER_REQUEST:
+        transaction = getTransaction(entry);
+        requestTransaction = transaction.getOpenRequestTransaction();
+        requestTransaction.setEnd(entry);
+        break;
+      case EXCEPTION:
+        transaction = getTransaction(entry);
+        transaction.addException(entry);
+        break;
+      case END:
+        transaction = getTransaction(entry);
+        transaction.setEnd(entry);
+        transactions.remove(entry.getCorrelationId());
+        break;
+      }
 
-			if (transaction != null) {
-				getXrayAgent().processTransaction(transaction);
-			}
+      if (transaction != null) {
+        getXrayAgent().processTransaction(transaction);
+      }
 
-			if (DEBUG_MODE)
-				logger.info("## Xray logged " + entry.getTrace());
-		}
-	}
+      if (DEBUG_MODE)
+        logger.info("## Xray logged " + entry.getTrace());
+    }
+  }
 
-	private XrayAgent getXrayAgent() {
-		if (this.xrayAgent == null) {
-			this.xrayAgent = XrayAgent.getInstance(this.awsRegion);
-		}
-		return this.xrayAgent;
-	}
+  private XrayAgent getXrayAgent() {
+    if (this.xrayAgent == null) {
+      this.xrayAgent = XrayAgent.getInstance(this.awsRegion);
+    }
+    return this.xrayAgent;
+  }
 
-	private JsonLoggerTransaction getTransaction(JsonLoggerEntry entry) {
-		Instant timeNow = Instant.now();
+  private JsonLoggerTransaction getTransaction(JsonLoggerEntry entry) {
+    Instant timeNow = Instant.now();
 
-		JsonLoggerTransaction transaction;
-		transaction = transactions.get(entry.getCorrelationId());
-		if (transaction == null) {
-			transaction = new JsonLoggerTransaction();
-			transactions.put(entry.getCorrelationId(), transaction);
-			transactionExpiry.put(timeNow.plusSeconds(EXPIRE_AFTER_SECONDS), entry.getCorrelationId());
-		}
+    JsonLoggerTransaction transaction;
+    transaction = transactions.get(entry.getCorrelationId());
+    if (transaction == null) {
+      transaction = new JsonLoggerTransaction();
+      transactions.put(entry.getCorrelationId(), transaction);
+      transactionExpiry.put(timeNow.plusSeconds(EXPIRE_AFTER_SECONDS), entry.getCorrelationId());
+    }
 
-		// Remove any expired transactions - this will keep our maps tidy.
-		SortedMap<Instant, String> exipredKeys = transactionExpiry.headMap(timeNow);
-		if (!exipredKeys.isEmpty()) {
-			if (DEBUG_MODE)
-				logger.debug("## Xray Purging keys: " + exipredKeys.values().stream().collect(Collectors.joining(",")));
-			exipredKeys.values().forEach(correlationId -> transactions.remove(correlationId));
+    // Remove any expired transactions - this will keep our maps tidy.
+    SortedMap<Instant, String> exipredKeys = transactionExpiry.headMap(timeNow);
+    if (!exipredKeys.isEmpty()) {
+      if (DEBUG_MODE)
+        logger.debug("## Xray Purging keys: " + exipredKeys.values().stream().collect(Collectors.joining(",")));
+      exipredKeys.values().forEach(correlationId -> transactions.remove(correlationId));
 
-			Set<Instant> expiredInstants = new HashSet<>();
-			expiredInstants.addAll(exipredKeys.keySet()); // Avoid Concurrent modification
-			expiredInstants.forEach(key -> transactionExpiry.remove(key));
-		}
+      Set<Instant> expiredInstants = new HashSet<>();
+      expiredInstants.addAll(exipredKeys.keySet()); // Avoid Concurrent modification
+      expiredInstants.forEach(key -> transactionExpiry.remove(key));
+    }
 
-		return transaction;
-	}
+    return transaction;
+  }
 
-	@Override
-	public void start() {
-		super.start();
-	}
+  @Override
+  public void start() {
+    super.start();
+  }
 
-	@Override
-	public void stop() {
-		super.stop();
-		getXrayAgent().stop();
-	}
+  @Override
+  public void stop() {
+    super.stop();
+    getXrayAgent().stop();
+  }
 
-	@Override
-	public String toString() {
-		return XrayAppender.class.getSimpleName();
+  @Override
+  public String toString() {
+    return XrayAppender.class.getSimpleName();
 
-	}
+  }
 
-	@PluginFactory
-	public static XrayAppender createXrayAppender(@PluginAttribute(value = "queueLength") Integer queueLength,
-			@PluginElement("Layout") Layout layout, @PluginAttribute(value = "awsRegion") String awsRegion,
-			@PluginAttribute(value = "name") String name,
-			@PluginAttribute(value = "ignoreExceptions", defaultBoolean = false) Boolean ignoreExceptions,
+  @PluginFactory
+  public static XrayAppender createXrayAppender(@PluginAttribute(value = "queueLength") Integer queueLength, @PluginElement("Layout") Layout layout,
+      @PluginAttribute(value = "awsRegion") String awsRegion, @PluginAttribute(value = "name") String name,
+      @PluginAttribute(value = "ignoreExceptions", defaultBoolean = false) Boolean ignoreExceptions,
 
-			@PluginAttribute(value = "messagesBatchSize") Integer messagesBatchSize) {
-		return new XrayAppender(name, layout, null, ignoreExceptions, awsRegion, queueLength, messagesBatchSize);
-	}
+      @PluginAttribute(value = "messagesBatchSize") Integer messagesBatchSize) {
+    return new XrayAppender(name, layout, null, ignoreExceptions, awsRegion, queueLength, messagesBatchSize);
+  }
 }
